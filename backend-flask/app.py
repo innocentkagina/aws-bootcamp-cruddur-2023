a9f4cd1769e90354493bdaf07625a4b049d452fe
innocentkagina <innocent.kanushu.kagina@gmail.com>
2023-03-01 22:41:59 +0000

    added rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,6 +15,11 @@ from services.show_activity import *
 from services.notifications_activities import *
 
 
+# Rollbar Libraries
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # AWS X-Ray Libraries
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
@@ -50,6 +55,24 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 app = Flask(__name__)
 
 # Initialize automatic instrumentation with Flask
@@ -74,6 +97,12 @@ def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
+
+#Rollbar
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
