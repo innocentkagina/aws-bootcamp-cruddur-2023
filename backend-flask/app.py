732da6bf601dbebb1c709896aab4c4e60bc59711
innocentkagina <innocent.kanushu.kagina@gmail.com>
2023-03-01 18:09:10 +0000

    minor update for cloudwatch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -89,7 +89,7 @@ def data_messages(handle):
   user_sender_handle = 'andrewbrown'
   user_receiver_handle = request.args.get('user_reciever_handle')
 
-  model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
+  model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle,logger=LOGGER)
   if model['errors'] is not None:
     return model['errors'], 422
   else:
